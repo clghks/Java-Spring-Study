0dac8a9e1e747dd9f45449832b8d3f13876d28e1
chihwan <clghks@quicket.co.kr>
2016-04-14 12:08:32 +0900

    어노테이션으로 서블릿 등록하기

---
+++ HelloServlet/src/com/clghks/helloservlet/HelloWorld2.java
@@ -0,0 +1,23 @@
+package com.clghks.helloservlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "HelloWorld2", urlPatterns = {"/ShowDate"})
+public class HelloWorld2 extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+	
+		PrintWriter printWriter = resp.getWriter();
+		printWriter.println("now = " + new Date());
+	}
+}
