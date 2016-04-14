18614c373092934d03b9568ed56212f6938b34a6
chihwan <clghks@quicket.co.kr>
2016-04-14 16:43:43 +0900

    나이계산 Tag 라이브러리 추가

--- HelloServlet/WebContent/hello.jsp
+++ HelloServlet/WebContent/hello.jsp
@@ -2,6 +2,7 @@
     pageEncoding="EUC-KR"%>
 
 <%@ taglib uri="http://www.clghks.co.kr/tags" prefix="ora"%>
+<%@ taglib uri="http://www.clghks.co.kr/ageTag" prefix="ora1" %>
 <%@ include file="param.jsp" %>    
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
@@ -25,10 +26,19 @@
 		x++;
 	%>
 	
+	<%
+		Calendar calendar = new GregorianCalendar();
+		calendar.set(1986, 9, 1);
+		
+		Date birth = calendar.getTime();
+	%>
+	
 	<%-- 출력? --%>
 	<h3>결과 : <%= x %></h3>
 	<h3>Test 결과 : <%=test() %> </h3>
 	
 	<h3><ora:helloTag message="안녕하세요."/></h3>
+	
+	<h3>나이: <ora1:ageTag birthday="<%=birth %>"/></h3>
 </body>
 </html>
---
+++ HelloServlet/src/com/clghks/helloservlet/AgeTag.java
@@ -0,0 +1,65 @@
+package com.clghks.helloservlet;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.PageContext;
+import javax.servlet.jsp.tagext.Tag;
+
+public class AgeTag implements Tag{
+	private PageContext pageContext;
+	private Tag parentTag;
+	private Date birthday;
+	
+	@Override
+	public void setPageContext(PageContext pageContext) {
+		this.pageContext = pageContext;
+	}
+	
+	@Override
+	public void setParent(Tag parentTag) {
+		this.parentTag = parentTag;
+	}
+
+	@Override
+	public int doStartTag() throws JspException {
+		try {
+			Calendar calendar = new GregorianCalendar();
+			calendar.setTime(new Date());
+			
+			Calendar birthDay = new GregorianCalendar();
+			birthDay.setTime(birthday);
+			
+			int age = calendar.get(Calendar.YEAR) - birthDay.get(Calendar.YEAR);
+			
+			JspWriter jspWriter = pageContext.getOut();
+			jspWriter.println(age + 1);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return SKIP_BODY;
+	}
+
+	@Override
+	public int doEndTag() throws JspException {
+		return EVAL_PAGE;
+	}
+
+	@Override
+	public Tag getParent() {
+		return parentTag;
+	}
+
+	@Override
+	public void release() {
+		
+	}
+	
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+
+}
