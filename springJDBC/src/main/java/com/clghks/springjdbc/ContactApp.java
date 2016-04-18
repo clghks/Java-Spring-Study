909dbd8bb8a787b13bd757a107ca3e7cb91e5499
chihwan <clghks@quicket.co.kr>
2016-04-18 16:07:32 +0900

    수

--- springJDBC/src/main/java/com/clghks/springjdbc/ContactApp.java
+++ springJDBC/src/main/java/com/clghks/springjdbc/ContactApp.java
@@ -7,8 +7,10 @@ public class ContactApp {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 		ContactsDao contactsDao = (ContactsDao) context.getBean("contactsDao");
+		
 		for(Contact contact : contactsDao.getContactsList()){
-			System.out.println("성 : " + contact.getFirstName() + ", 이름 : " + contact.getLastName());
+			System.out.println("이름 : " + contact.getFirstName() + " " + contact.getLastName());
 		}
+		
 	}
 }
