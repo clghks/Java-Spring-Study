df906a2c8a2afdfec8775cbdf5d8e96cd486a984
chihwan <clghks@quicket.co.kr>
2016-04-19 11:47:16 +0900

    test

--- SpringMVC/src/main/java/com/clghks/spring/BaseboallController.java
+++ SpringMVC/src/main/java/com/clghks/spring/BaseboallController.java
@@ -8,7 +8,27 @@ public class BaseboallController {
 	
 	@RequestMapping("/baseball")
 	public String index(){
-		return "/baseball/index";
+		return "forward:/baseball2";
+	}
+	
+	@RequestMapping("/baseball1")
+	public String baseball1(){
+		return "redirect:/baseball3";
+	}
+	
+	@RequestMapping("/baseball4")
+	public String baseball4(){
+		return "redirect:http://www.naver.com";
+	}
+	
+	@RequestMapping("baseball2")
+	public String baseball2(){
+		return "/baseball/index2";
+	}
+	
+	@RequestMapping("baseball3")
+	public String baseball3(){
+		return "/baseball/index3";
 	}
 	
 }
