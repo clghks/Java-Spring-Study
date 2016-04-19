74dfc8aa2e75839b1fdc6c901ccb26c1ea2c3158
chihwan <clghks@quicket.co.kr>
2016-04-19 10:58:58 +0900

    spring mvc

---
+++ SpringMVC/src/main/java/com/clghks/spring/HelloWorldController.java
@@ -0,0 +1,21 @@
+package com.clghks.spring;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloWorldController {
+
+	@RequestMapping("/hello")
+	public ModelAndView helloworld(){
+		ModelAndView mav = new ModelAndView("/helloview");
+
+		mav.addObject("message1", "<strong>Spring mvc</strong>");
+		mav.getModelMap().put("message2", "메시지2");
+		mav.addObject("currentDate", new Date());
+		return mav;
+	}
+}
