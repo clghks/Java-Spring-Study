db061fba087bbfd6ca4a1a95b25f46031863cc9e
chihwan <clghks@quicket.co.kr>
2016-04-20 11:20:03 +0900

    인코딩 관련

--- SpringMVC/src/main/java/com/clghks/spring/JsonController.java
+++ SpringMVC/src/main/java/com/clghks/spring/JsonController.java
@@ -12,13 +12,21 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class JsonController {
 	
 	@RequestMapping("/jsonOut")
-//	@RequestMapping(value="/jsonOut",  produces="application/json")
+//	@RequestMapping(value="/jsonOut",  produces="application/json;charset=UTF-8")
 	@ResponseBody
 	public List<UserInfoDto> jsonOut(){
 		List<UserInfoDto> userInfoDtos = new ArrayList<UserInfoDto>();
 		userInfoDtos.add(new UserInfoDto("영업", "홍길동1"));
 		userInfoDtos.add(new UserInfoDto("영업", "홍길동2"));
-		userInfoDtos.add(new UserInfoDto("영업", "홍길동3"));
+		userInfoDtos.add(new UserInfoDto("기획", "홍길동3"));
+		userInfoDtos.add(new UserInfoDto("기획", "홍길동4"));
+		userInfoDtos.add(new UserInfoDto("기획", "홍길동5"));
+		userInfoDtos.add(new UserInfoDto("개발", "홍길동6"));
+		userInfoDtos.add(new UserInfoDto("개발", "홍길동7"));
+		userInfoDtos.add(new UserInfoDto("개발", "홍길동8"));
+		userInfoDtos.add(new UserInfoDto("개발", "홍길동9"));
+		userInfoDtos.add(new UserInfoDto("영업", "홍길동10"));
+		userInfoDtos.add(new UserInfoDto("영업", "홍길동11"));
 		
 		return userInfoDtos;
 	}
